c8c79d87e1b4302aae90614ccae52bff2e08083d
cotugame <cotugame@tutanota.com>
2020-09-21 17:57:42 +0000

    マウス位置にrect移動

--- script/main.js
+++ script/main.js
@@ -1,22 +1,46 @@
+'use strict'
+
 function main(param) {
-    var scene = new g.Scene({ game: g.game });
-    scene.loaded.add(function () {
-        // 以下にゲームのロジックを記述します。
-        var rect = new g.FilledRect({
-            scene: scene,
-            cssColor: "#ff0000",
-            width: 32,
-            height: 32
-        });
-        rect.update.add(function () {
-            // 以下のコードは毎フレーム実行されます。
-            rect.x++;
-            if (rect.x > g.game.width)
-                rect.x = 0;
-            rect.modified();
-        });
-        scene.append(rect);
-    });
-    g.game.pushScene(scene);
+	const scene = new g.Scene({ game: g.game });
+	scene.loaded.add(function () {
+		const characters = {}
+
+		const playerMain = (ev, delta = true) => {
+			const playerId = ev.player.id
+			const x = ev.point.x+(delta ? ev.startDelta.x : 0)
+			const y = ev.point.y+(delta ? ev.startDelta.y : 0)
+			if (characters[playerId] == null) {
+				characters[playerId] = {} 
+				characters[playerId].x = x
+				characters[playerId].y = y
+				const rect = new g.FilledRect({
+					scene: scene,
+					cssColor: "#ff0000",
+					width: 32,
+					height: 32
+				})
+				rect.update.add(() => {
+					rect.x = characters[playerId].x
+					rect.y = characters[playerId].y
+					rect.modified()
+				})
+				scene.append(rect)
+			} else {
+				characters[playerId].x = x
+				characters[playerId].y = y
+			}
+		}
+
+		scene.pointDownCapture.add((ev) => {
+			playerMain(ev, false)
+		})
+		scene.pointMoveCapture.add((ev) => {
+			playerMain(ev)
+		})
+		scene.pointUpCapture.add((ev) => {
+			playerMain(ev)
+		})
+	});
+	g.game.pushScene(scene);
 }
 module.exports = main;
